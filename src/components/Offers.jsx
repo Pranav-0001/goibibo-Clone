73b0bcce591b6868ae23c465e9ad5b625faf7801
Pranav-0001 <pranavchakkalakkal@gmail.com>
2023-10-26 16:53:31 +0530

    added scroll button

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { hotels } from '../constants'
 import Confirm from './Confirm'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const Offers = () => {
     const [confirm,setConfirm]=useState(false)
     const [hotel,setHotel]=useState({})
+    const containerRef = useRef(null);
+    const scrollRight = () => {
+        if (containerRef.current) {
+          containerRef.current.scrollLeft += 300; // Adjust the scroll distance as needed
+        }
+      }; 
 
+      const scrollLeft = () => {
+        if (containerRef.current) {
+          containerRef.current.scrollLeft -= 300; // Adjust the scroll distance as needed
+        }
+      };
     return (
         <>
         <div className='w-full flex justify-center pt-10  '>
@@ -26,7 +39,7 @@ const Offers = () => {
 
                     </div>
                 </div>
-                <div className='w-full   px-4 flex overflow-scroll gap-2 pb-8 pt-8'>
+                <div className='w-full   px-4 flex overflow-scroll gap-2 pb-8 pt-8 scroll-smooth' ref={containerRef}>
                 {hotels.map(item=><div className='p-2 border min-w-[300px]'>
                     <img src={item.image} alt="" />
                     <h1 className='font-bold'>{item.name}</h1>
@@ -34,6 +47,10 @@ const Offers = () => {
                     <p>{item.description}</p>
                     <button onClick={()=>{setConfirm(true);setHotel(item)}} className='mt-4 px-2 py-2 text-white shadow rounded bg-[#ff6d38] flex justify-center items-center'>Book Now</button>
                 </div>)}
+                </div>
+                <div className='flex gap-1 justify-end pb-2'>
+                    <FontAwesomeIcon onClick={scrollLeft} className='p-3 bg-orange-500 text-white rounded-full' icon={faArrowLeft}/>
+                    <FontAwesomeIcon onClick={scrollRight} className='p-3  bg-orange-500 text-white rounded-full' icon={faArrowRight}/>
                 </div>
             </div>
             {confirm&&<Confirm hotel={hotel} close={setConfirm}/>}
